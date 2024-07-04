06f4defdd963dfd824d5932827af22c59f442e49
Azizbek <webcoderazizbek@gmail.com>
2024-07-04 03:54:51 -0700

    python-dars

---
+++ 1-dars.py
@@ -0,0 +1,12 @@
+
+# calculator dasturi
+a = input('birrinchi sonni kiriting')
+b = input('ikkinchi sonni kiriting')
+
+c = int(a) + int(b)
+print('natija', c)
+
+# soat dasturi
+from datetime import datetime
+bugun = datetime.now()
+print(bugun)
