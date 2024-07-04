bd5562e45397993514cc3c057573dc75ed7d08cc
Azizbek <webcoderazizbek@gmail.com>
2024-07-04 11:22:00 -0700

    fff

---
+++ 2-dars.py
@@ -0,0 +1,18 @@
+video_nome = 'python dasturlash video'
+
+soni = 23
+
+strint = '10'
+
+print(strint)
+
+a = 5
+b = 10
+
+c = a + b
+print(c)
+
+a = input('birrinchi sonni kiriting ')
+b = input('ikkinchi sonni kiriting ')
+c = int(a) + int(b)
+print('natija',c)
